27e037a9f54da310fb1b4ba8c83bffbb82482dfb
Fawcett595 <kevinfawcett88web@gmail.com>
2013-08-01 19:40:22 -0500

    Adding unit testing

---
+++ Game/test/core/schedulerTest.js
@@ -0,0 +1,21 @@
+require(["Utility"], function (Utility)
+{
+    UtilityTest = TestCase("UtilityTest");
+
+    UtilityTest.prototype.setUp = function()
+    {
+
+    }
+
+    UtilityTest.prototype.tearDown = function()
+    {
+
+    }
+
+    UtilityTest.prototype.testRemoveElement = function()
+    {
+        var input = [1, 2];
+        Utility.removeElement(input, 2);
+        assertEquals(1, input.length);
+    }
+});
