fe2c2483afea1288810ca1144578078e44c3a358
Royce <coffeegoblins@gmail.com>
2014-02-13 20:46:54 -0600

    Fixing the build config

--- build/buildRemote.js
+++ build/buildRemote.js
@@ -1,20 +1,19 @@
 ({
-    appDir: "../",
-    baseUrl: "./",
-    dir: "../build output",
+    appDir: '../Client',
+    baseUrl: './',
+    dir: '../build output',
     paths: {
         require: 'lib/require',
-        i18n: 'lib/i18n',
         text: 'lib/text',
         renderer: 'Renderer/src/renderer',
         jsonLoader: 'Core/src/functions/loadRemoteJSON'
     },
     optimize: 'uglify2',
-    optimizeCss: "standard",
+    optimizeCss: 'standard',
     inlineText: true,
     useStrict: true,
     modules: [
-        {name: "main"}
+        {name: 'main'}
     ],
     fileExclusionRegExp: /\.git/
 })
