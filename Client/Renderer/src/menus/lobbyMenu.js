a1a85ad5d46b02a726acaebe773a27474028efc9
coffeegoblins <coffeegoblins@gmail.com>
2014-03-08 07:51:39 -0600

    Lobby Menu Slider

--- Client/Renderer/src/menus/lobbyMenu.js
+++ Client/Renderer/src/menus/lobbyMenu.js
@@ -10,11 +10,27 @@ define(['text!Renderer/content/templates/lobbyMenu.html', 'Core/src/inputHandler
         {
             document.body.backgroundColor = "#FFF";
             document.body.innerHTML = Template;
+
+            this.menuBar = document.getElementById('menuBar');
+
+            InputHandler.registerClickEvent('navBar', this.onNavButtonClicked, this);
+            InputHandler.registerClickEvent('navButton', this.onNavButtonClicked, this);
         };
 
         LobbyMenu.prototype.hide = function ()
         {
-            document.body.innerHTML = "";
+            document.body.innerHTML = '';
+        };
+
+        LobbyMenu.prototype.onNavButtonClicked = function (e)
+        {
+            if (this.menuBar.className === 'active')
+            {
+                this.menuBar.className = 'inactive';
+                return;
+            }
+
+            this.menuBar.className = 'active';
         };
 
         return new LobbyMenu();
