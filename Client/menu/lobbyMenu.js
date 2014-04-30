0d9fd9017bd1e7cefe828c517fc0c18216059a27
coffeegoblins <coffeegoblins@gmail.com>
2014-04-29 20:27:39 -0500

    Moving project structure around

--- Client/menu/lobbyMenu.js
+++ Client/menu/lobbyMenu.js
@@ -4,21 +4,21 @@ define(['text!Renderer/content/templates/lobbyMenu.html', 'text!Renderer/content
         function LobbyMenu(socket)
         {
             this.socket = socket;
-            
+
             this.socket.on('search_succeeded', function (cursor)
             {
                 for (var x = this.searchResultsTable.rows.length - 1; x >= 0; --x)
                 {
                     this.searchResultsTable.deleteRow(x);
                 }
-                
+
                 for (var i = 0; i < cursor.length; ++i)
                 {
                     var row = this.searchResultsTable.insertRow(i);
-                    
+
                     var cell1 = row.insertCell(0);
                     var cell2 = row.insertCell(1);
-                    
+
                     cell1.innerHTML = cursor[i].username;
                     cell1.className = "userName";
                     cell2.innerHTML = "<input type='button' value='Challenge!'>";
@@ -29,14 +29,14 @@ define(['text!Renderer/content/templates/lobbyMenu.html', 'text!Renderer/content
         LobbyMenu.prototype.show = function ()
         {
             document.body.innerHTML = LobbyTemplate;
-            
+
             document.getElementById('notificationsButton').addEventListener('click', this.onNotificationButtonClicked.bind(this));
             document.getElementById('waitingOnYouButton').addEventListener('click', this.onWaitingOnYouButtonClicked.bind(this));
             document.getElementById('waitingOnThemButton').addEventListener('click', this.onWaitingOnThemButtonClicked.bind(this));
             document.getElementById('playerSearchButton').addEventListener('click', this.onPlayerSearchButtonClicked.bind(this));
-            
+
             this.content = document.getElementById('content');
-            
+
             this.mainMenuChains = document.getElementById('mainMenuChains');
             this.mainMenuChains.className = 'lowerChains';
         };
@@ -52,25 +52,25 @@ define(['text!Renderer/content/templates/lobbyMenu.html', 'text!Renderer/content
             this.content.innerHTML += PlayerSearchTemplate;
             this.searchCriteria = document.getElementById('searchCriteria');
             this.searchResultsTable = document.getElementById('searchResultsTable');
-            
+
             document.getElementById('searchButton').addEventListener('click', this.onSearchButtonClicked.bind(this));
         };
-        
+
         LobbyMenu.prototype.onNotificationButtonClicked = function ()
         {
-            
+
         };
-        
+
         LobbyMenu.prototype.onWaitingOnYouButtonClicked = function ()
         {
-            
+
         };
-        
+
         LobbyMenu.prototype.onWaitingOnThemButtonClicked = function ()
         {
-            
+
         };
-        
+
         LobbyMenu.prototype.onSearchButtonClicked = function ()
         {
             this.socket.emit('playerSearch', this.searchCriteria.value);
