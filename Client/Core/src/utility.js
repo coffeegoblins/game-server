23aaa1c0b46ed5ed9b9b6e965c26f36d8ebb91bb
Royce <coffeegoblins@gmail.com>
2014-05-20 00:28:34 -0500

    Making a few modifications to the unit selection screen

--- Client/Core/src/utility.js
+++ Client/Core/src/utility.js
@@ -1,8 +1,8 @@
 define(function ()
 {
     'use strict';
-    return {
 
+    return {
         containsElement: function (array, element)
         {
             for (var i = 0; i < array.length; i++)
--- Client/menu/unitSelection.js
+++ Client/menu/unitSelection.js
@@ -17,22 +17,40 @@ define(['text!./unitSelection.html', 'core/src/events', 'core/src/utility'], fun
         var contentElement = document.getElementById('content');
         Utility.insertTemplate(contentElement, Template);
 
+        this.element = contentElement.querySelector('.unit-selection');
         this.confirmButton = contentElement.querySelector('[data-button="confirm"');
         //confirmButton.disabled = true;
 
-        this.confirmButton.addEventListener('click', function ()
-        {
-            this.trigger('confirm');
-        }.bind(this), false);
-
-        contentElement.querySelector('[data-button="cancel"').addEventListener('click', function ()
-        {
-            this.trigger('cancel');
-        }.bind(this), false);
-
+        this.confirmButton.addEventListener('click', this.onConfirm.bind(this), false);
+        contentElement.querySelector('.tab-header').on('click', '.unit-type', this.onTabClick.bind(this));
+        contentElement.querySelector('[data-button="cancel"').addEventListener('click', this.onCancel.bind(this), false);
         return this;
     };
 
+    UnitSelection.prototype.onCancel = function ()
+    {
+        this.trigger('cancel');
+    };
+
+    UnitSelection.prototype.onConfirm = function ()
+    {
+        this.trigger('confirm');
+    };
+
+    UnitSelection.prototype.onTabClick = function (e)
+    {
+        var tab = Utility.findParentElement(e.target, '.tab');
+        if (tab.classList.contains('selected'))
+            return;
+
+        var selectedItems = this.element.querySelectorAll('.selected');
+        for (var i = 0; i < selectedItems.length; i++)
+            selectedItems[i].classList.remove('selected');
+
+        tab.classList.add('selected');
+        var unitType = tab.getAttribute('data-unit');
+        this.element.querySelector('.tab-content[data-unit="' + unitType + '"]').classList.add('selected');
+    };
 
     Events.register(UnitSelection.prototype);
     return UnitSelection;
