aed54366d2b817e4c986579e3ef5b9064b9854a5
coffeegoblins <coffeegoblins@gmail.com>
2014-07-03 20:31:42 -0500

    GetLevel and GetLevels calls

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ var UserManager = require('./userManager');
 var NotificationManager = require('./notificationManager');
 var ChallengeManager = require('./challengeManager');
 var GameManager = require('./gameManager');
+var LevelManager = require('./levelManager');
 
 var fileSystem = require('fs');
 var atob = require('atob');
@@ -39,6 +40,7 @@ databaseManager.open(config.dbName, config.dbHost, config.dbPort, function ()
     var notificationManager = new NotificationManager(events);
     var challengeManager = new ChallengeManager(events);
     var gameManager = new GameManager(events);
+    var levelManager = new LevelManager(events);
 
     io.sockets.on('connection', function (socket)
     {
@@ -55,15 +57,19 @@ databaseManager.open(config.dbName, config.dbHost, config.dbPort, function ()
             socket.username = username;
 
             socket.on(events.searchByUsername.name, userManager.selectPlayers.bind(userManager, responseCallback));
-            socket.on(events.gameStateUpdate.name, function () {}.bind(userManager, responseCallback));
 
             socket.on(events.getNotifications.name, notificationManager.getNotifications.bind(notificationManager, responseCallback, socket.username));
+
             socket.on(events.getGames.name, gameManager.getGames.bind(gameManager, responseCallback, socket.username));
             socket.on(events.getGameLogic.name, gameManager.getGameLogic.bind(gameManager, responseCallback));
+            socket.on(events.gameStateUpdate.name, function () {}.bind(userManager, responseCallback));
 
             socket.on(events.challengeUser.name, challengeManager.initiateChallenge.bind(challengeManager, responseCallback, socket.username));
             socket.on(events.challengeAccepted.name, challengeManager.acceptChallenge.bind(challengeManager, responseCallback, socket.username));
             socket.on(events.challengeDeclined.name, challengeManager.removeChallenge.bind(challengeManager, responseCallback, socket.username));
+
+            socket.on(events.getLevel.name, levelManager.getLevels.bind(levelManager, responseCallback));
+            socket.on(events.getLevels.name, levelManager.getLevels.bind(levelManager, responseCallback));
         }
 
         socket.on(events.login.name, userManager.login.bind(userManager, responseCallback, onLoginSucceeded));
