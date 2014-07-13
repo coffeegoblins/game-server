93e77ef159282e4ab5fd394340ce3969e0d91e4b
Royce <coffeegoblins@gmail.com>
2014-07-12 22:44:26 -0500

    Getting the game loading from multiplayer again

--- Client/Core/src/remoteJSONLoader.js
+++ Client/Core/src/remoteJSONLoader.js
@@ -9,8 +9,11 @@ define([], function ()
 
     RemoteJSONLoader.prototype.loadLevel = function (levelName, onComplete)
     {
-        this.socket.emit(this.socket.events.getLevel, levelName);
-        this.socket.on(this.socket.events.getLevel.response.success, onComplete);
+        this.socket.emit(this.socket.events.getLevel.name, levelName);
+        this.socket.on(this.socket.events.getLevel.response.success, function (level)
+        {
+            onComplete(level.data);
+        });
     };
 
     return RemoteJSONLoader;
